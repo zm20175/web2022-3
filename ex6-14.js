74d61e6005012e586bac280918c11409248d9b2e
zm20175 <103973938+zm20175@users.noreply.github.com>
2023-01-18 18:19:09 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
 function imgchange5() {document.getElementById('logo1').src ="./image/keiei.jfif";}
-function imgchange6() {document.getElementById('logo1').src ="./image/josai.jfif";}
+function imgchange6() {document.getElementById('logo1').src ="./image/jousai.jfif";}
