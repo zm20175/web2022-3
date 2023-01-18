41e5bff8b754cb679238ad49846974714751c349
zm20175 <103973938+zm20175@users.noreply.github.com>
2023-01-18 17:01:33 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,19 +1,20 @@
 function hyoji() {
   //表示する文字
-  
   varstr= "一文字ずつ表示します。";
+  
   //テキストボックスの文字数
-  
   varcnt= document.timer.moji.value.length;
+ 
   //文字が全部表示されているか確認
-  
-  
-if ( cnt< 11 ) {
+  if (cnt< 11) {
+
 //現在より1文字多く切り出して表示
+document.timer.moji.value= str.substr(0,cnt+1); } 
   
-document.timer.moji.value= str.substr(0,cnt+1); } else {
+else {
 //全て表示されたら、空文字に戻す
-  
-document.timer.moji.value= ""; } } function startfnc() {
+document.timer.moji.value= ""; } }
+
+function startfnc() {
 //関数hyoji()を1000ミリ秒間隔で呼び出す
 setInterval("hyoji()",1000); } 
